4fa0e316d573568c3a0c0c7ea34b372f0c373f58
Kenny <kshultz@permitzip.com>
2022-03-15 10:16:58 -0400

    added some scripts for testnet build

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -55,7 +55,17 @@ pub fn development_config() -> Result<ChainSpec, String> {
 				// Pre-funded accounts
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
 					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
 				],
 				true,
 			)
@@ -80,70 +90,31 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 
 	Ok(ChainSpec::from_genesis(
 		// Name
-		"Local Testnet Mod",
+		"Build3 Local Testnet",
 		// ID
-		"local_testnet",
+		"build3_local_testnet",
 		ChainType::Local,
-		move || -> GenesisConfig {
+		move || {
 			testnet_genesis(
 				wasm_binary,
 				// Initial PoA authorities
-				vec![
-					authority_keys_from_seed("Kenny"),
-					authority_keys_from_seed("Jayce"),
-					authority_keys_from_seed("Simon"),
-					authority_keys_from_seed("Phil"),
-					authority_keys_from_seed("Colby"),
-				],
+				vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
 				// Sudo account
-				get_account_id_from_seed::<sr25519::Public>("Kenny"),
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Kenny"),
-					get_account_id_from_seed::<sr25519::Public>("Jayce"),
-					get_account_id_from_seed::<sr25519::Public>("Simon"),
-					get_account_id_from_seed::<sr25519::Public>("Phil"),
-					get_account_id_from_seed::<sr25519::Public>("Colby"),
-					get_account_id_from_seed::<sr25519::Public>("Ricardo"),
-					get_account_id_from_seed::<sr25519::Public>("Ben"),
-					get_account_id_from_seed::<sr25519::Public>("Midhun"),
-					get_account_id_from_seed::<sr25519::Public>("MV"),
-					get_account_id_from_seed::<sr25519::Public>("Herin"),
-					get_account_id_from_seed::<sr25519::Public>("Gokul"),
-					get_account_id_from_seed::<sr25519::Public>("Binoy"),
-					get_account_id_from_seed::<sr25519::Public>("Ramdas"),
-					get_account_id_from_seed::<sr25519::Public>("Shifin"),
-					get_account_id_from_seed::<sr25519::Public>("Akhil"),
-					get_account_id_from_seed::<sr25519::Public>("Sreejith"),
-					get_account_id_from_seed::<sr25519::Public>("Arun"),
-					get_account_id_from_seed::<sr25519::Public>("Carter"),
-					get_account_id_from_seed::<sr25519::Public>("Praveen"),
-					get_account_id_from_seed::<sr25519::Public>("Rathika"),
-					get_account_id_from_seed::<sr25519::Public>("Sidney"),
-					get_account_id_from_seed::<sr25519::Public>("SteveK"),
-					get_account_id_from_seed::<sr25519::Public>("Suraj"),
-					get_account_id_from_seed::<sr25519::Public>("Kenny//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Jayce//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Simon//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Phil//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Colby//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ricardo//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ben//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Midhun//stash"),
-					get_account_id_from_seed::<sr25519::Public>("MV//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Herin//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Gokul//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Binoy//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Ramdas//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Shifin//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Akhil//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Sreejith//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Arun//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Carter//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Praveen//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Rathika//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Sidney//stash"),
-					get_account_id_from_seed::<sr25519::Public>("Suraj//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Alice"),
+					get_account_id_from_seed::<sr25519::Public>("Bob"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie"),
+					get_account_id_from_seed::<sr25519::Public>("Dave"),
+					get_account_id_from_seed::<sr25519::Public>("Eve"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+					get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
 				],
 				true,
 			)
@@ -153,7 +124,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		// Telemetry
 		None,
 		// Protocol ID
-		Some("rpc"),
+		Some("build3-local-testnet"),
 		// Fork ID
 		None,
 		// Properties
