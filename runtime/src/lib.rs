80a822974dafe4a822c32553dee4166c8f146fab
Kenny <kshultz@permitzip.com>
2022-03-15 21:45:15 -0400

    adding dependency

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -26,6 +26,8 @@ use sp_std::prelude::*;
 use sp_version::NativeVersion;
 use sp_version::RuntimeVersion;
 
+// use pallet_identity::*;
+
 // A few exports that help ease life for downstream crates.
 pub use frame_support::{
 	construct_runtime, parameter_types,
